5bbec2b3044cf472206c508f467e9386c0cffbfe
Bilal <bilalshaikh370@gmail.com>
2020-02-21 11:21:03 -0500

    Completed

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    [SerializeField] private GameObject itemFeedback;
+
+    private bool hasBeenCollected = false;
+    private bool isColided = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasBeenCollected)
+        {
+            isColided = true;
+           
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            hasBeenCollected = true;
+        }
+
+        if (hasBeenCollected)
+        {
+            Instantiate(itemFeedback, this.transform.position, this.transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/StarController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(transform.position.x, Random.Range(10, 15)));
+    }
+}
